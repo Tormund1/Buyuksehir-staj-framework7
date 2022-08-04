211d455a815a6595be40de35aee63b8776b2d0b9
Tormund1 <77021610+Tormund1@users.noreply.github.com>
2022-08-04 14:08:56 +0300

    first commit

---
+++ www/js/routes.js
@@ -0,0 +1,35 @@
+var routes = [
+  {
+    path: "/",
+    url: "./index.html",
+    name: "index",
+  },
+  {
+    path: "/kayit",
+    url: "./kayit.html",
+  },
+  {
+    path: "/home",
+    url: "./home.html",
+  },
+  {
+    path: "/searchScreen",
+    url: "./searchScreen.html",
+  },
+  {
+    path: "/kayit",
+    url: "kayit.html",
+  },
+  {
+    path: "/profil",
+    url: "./profil.html",
+  },
+  {
+    path: "/anaEkran",
+    url: "./anaEkran.html",
+  },
+  {
+    path: "/profileSetting",
+    url: "./profileSetting.html",
+  },
+];
